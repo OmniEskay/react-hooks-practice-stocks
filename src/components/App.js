232418b7698f4660ed417a9d4d85a2dc155387e3
+ <omnieskay@proton.me>
2025-04-27 21:07:13 +0300

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,54 @@
-import React from "react";
-import Header from "./Header";
-import MainContainer from "./MainContainer";
+import React, { useState, useEffect } from "react";
+import StockContainer from "./StockContainer";
+import PortfolioContainer from "./PortfolioContainer";
+import SearchBar from "./SearchBar";
 
 function App() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [sortBy, setSortBy] = useState(""); 
+  const [filterBy, setFilterBy] = useState(""); 
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks") 
+      .then((res) => res.json())
+      .then((data) => setStocks(data));
+  }, []);
+
+  function handleBuyStock(stock) {
+    if (!portfolio.includes(stock)) {
+      setPortfolio([...portfolio, stock]);
+    }
+  }
+
+  function handleSellStock(stock) {
+    setPortfolio(portfolio.filter((s) => s.id !== stock.id));
+  }
+
+  const sortedStocks = [...stocks].sort((stockA, stockB) => {
+    if (sortBy === "Alphabetically") {
+      return stockA.name.localeCompare(stockB.name);
+    } else if (sortBy === "Price") {
+      return stockA.price - stockB.price;
+    } else {
+      return 0; 
+    }
+  });
+
+  const visibleStocks = filterBy
+    ? sortedStocks.filter((stock) => stock.type === filterBy)
+    : sortedStocks;
+
   return (
     <div>
-      <Header />
-      <MainContainer />
+      <SearchBar 
+        sortBy={sortBy} 
+        onSortChange={setSortBy} 
+        filterBy={filterBy} 
+        onFilterChange={setFilterBy} 
+      />
+      <StockContainer stocks={visibleStocks} onStockClick={handleBuyStock} />
+      <PortfolioContainer portfolio={portfolio} onStockClick={handleSellStock} />
     </div>
   );
 }
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [sortBy, setSortBy] = useState(""); 
+  const [filterBy, setFilterBy] = useState(""); 
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks") 
+      .then((res) => res.json())
+      .then((data) => setStocks(data));
+  }, []);
+
+  function handleBuyStock(stock) {
+    if (!portfolio.includes(stock)) {
+      setPortfolio([...portfolio, stock]);
+    }
+  }
+
+  function handleSellStock(stock) {
+    setPortfolio(portfolio.filter((s) => s.id !== stock.id));
+  }
+
+  const sortedStocks = [...stocks].sort((a, b) => {
+    if (sortBy === "Alphabetically") {
+      return a.name.localeCompare(b.name);
+    } else if (sortBy === "Price") {
+      return a.price - b.price;
+    } else {
+      return 0; 
+    }
+  });
+
+  const visibleStocks = filterBy
+    ? sortedStocks.filter((stock) => stock.type === filterBy)
+    : sortedStocks;
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar 
+        sortBy={sortBy}
+        onSortChange={setSortBy}
+        filterBy={filterBy}
+        onFilterChange={setFilterBy}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer 
+            stocks={visibleStocks} 
+            onStockClick={handleBuyStock} 
+          />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer 
+            portfolio={portfolio} 
+            onStockClick={handleSellStock} 
+          />
         </div>
       </div>
     </div>
